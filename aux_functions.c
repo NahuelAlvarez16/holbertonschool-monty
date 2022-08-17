9c0c9e31c62b978e6bad5dc5d069c9f12a14016d
Juan Rey <5079@holbertonschool.com>
2022-08-17 06:39:29 -0700

    add _strdup

--- aux_functions.c
+++ aux_functions.c
@@ -101,3 +101,33 @@ char **generate_arguments(char *s, char *delimeters)
 	free(s_tmp);
 	return (args);
 }
+
+/**
+ * _strdup - pointer to a newly allocated space in memory
+ * @str: string
+ * Return: return array
+ */
+char *_strdup(char *str)
+{
+	char *temp;
+	unsigned int i = 0;
+	unsigned int size = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[size]; size++)
+		;
+
+	temp = malloc(size + 1);
+
+	if (temp == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		temp[i] = str[i];
+		i++;
+	}
+	temp[i] = '\0';
+	return (temp);
+}
