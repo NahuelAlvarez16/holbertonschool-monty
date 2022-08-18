125a53063ec91ef9b6e2a4dcdbbfff785a788a15
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2022-08-18 13:01:23 -0700

    Fix some details in _swap function.

--- opcode_functions.c
+++ opcode_functions.c
@@ -87,7 +87,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 
 	if (!(*stack) || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		free_list(*stack);
 		fclose(file);
 		exit(EXIT_FAILURE);
